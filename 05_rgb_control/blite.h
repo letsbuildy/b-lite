14d4c621c30f001e3e29edc48471a3f8488e8c09
Sayan Paul <saypaul@redhat.com>
2023-04-10 13:54:54 +0530

    working codes

---
+++ 05_rgb_control/blite.h
@@ -0,0 +1,29 @@
+#ifndef BLITE_H
+#define BLITE_H
+
+//Motor pins(2 X 2-pin connector) connected to DRR8833
+#define m1_pin1 2
+#define m1_pin2 0
+#define m2_pin1 10
+#define m2_pin2 14
+
+// programmable I/O (2 X 3-pin connector) 5v bi-directional level shifted driving at BAT level
+#define io1 12
+#define io2 13
+
+// Analog input pin (1 X 3-pin connector) driving at 3.3V
+#define analog_sensor A0
+
+// I2C (1X 4-pin connector) driving at BAT level
+#define i2c_tx 4
+#define i2c_rx 5
+
+//HC-SR04 pin
+#define io3 4
+#define io4 5
+
+// pre-soldered components in board  
+#define buzzer 15
+#define switch 16
+
+#endif
---
+++ tests/blite.h
@@ -0,0 +1,25 @@
+#ifndef BLITE_H
+#define BLITE_H
+
+//Motor pins(2 X 2-pin connector) connected to DRR8833
+#define m1_pin1 2
+#define m1_pin2 0
+#define m2_pin1 10
+#define m2_pin2 14
+
+// programmable I/O (2 X 3-pin connector) 5v bi-directional level shifted driving at BAT level
+#define io1 12
+#define io2 13
+
+// Analog input pin (1 X 3-pin connector) driving at 3.3V
+#define analog_sensor A0
+
+// I2C/HC-SR04 pin (1X 4-pin connector) driving at BAT level
+#define i2c_tx 4
+#define i2c_rx 5
+
+// pre-soldered components in board  
+#define buzzer 15
+#define switch 16
+
+#endif
