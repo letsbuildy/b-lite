4c47c306206898ff09c30a393f49d6322dea87d6
Sayan Paul <saypaul@saypaul-thinkpadx1carbongen9.punetw6.csb>
2024-03-10 14:31:25 +0530

    formatting fix

--- example/14_blite_rc_car/14_blite_rc_car.ino
+++ example/14_blite_rc_car/14_blite_rc_car.ino
@@ -8,12 +8,13 @@ void setup(){
     myBot.setup();
     Serial.begin(115200);
     myBot.APServer();
+
     server.on("/", HTTP_GET, []() {
-    Serial.println("Web Server: received a web page request");
-    String html = HTML_CONTENT;  // Use the HTML content from the servo_html.h file
-    server.send(200, "text/html", html);
-  });
-  server.begin();
+        Serial.println("Web Server: received a web page request");
+        String html = HTML_CONTENT;
+        server.send(200, "text/html", html);
+    });
+    server.begin();
 }
 void loop(){
     server.handleClient();
