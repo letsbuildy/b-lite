b7940219c4dbabf1a8ad9d84faf4f5cdf1aa0173
Sayan Paul <paul.sayan@gmail.com>
2024-03-13 01:15:02 +0530

    modifying 14 code

--- examples/14_blite_rc_car/14_blite_rc_car.ino
+++ examples/14_blite_rc_car/14_blite_rc_car.ino
@@ -2,21 +2,16 @@
 #include "remote.h"
 
 Blite myBot;
-ESP8266WebServer wifiRemoteControl(80);
 
 void setup(){
     myBot.setup();
     Serial.begin(115200);
     myBot.smartConnectWiFi();
-    wifiRemoteControl.on("/", HTTP_GET, []() {
-        Serial.println("Web Server: received a web page request");
-        String html = REMOTE_HTML_CONTENT;
-        wifiRemoteControl.send(200, "text/html", html);
-    });
-    wifiRemoteControl.begin();
+
 }
 void loop(){
-    wifiRemoteControl.handleClient();
+    String html = REMOTE_HTML_CONTENT;
+    myBot.smartRenderServer(html);
     if (myBot.buttonPressed()){
         myBot.blinkLed(2);
     }
