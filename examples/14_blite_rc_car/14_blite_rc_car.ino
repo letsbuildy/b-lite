130fbd25a5682180b15e5b34be6e75429c1b3b7a
unknown <hidg0410@gmail.com>
2024-03-14 21:46:32 +0530

    commited by Debasish Goswami

--- examples/14_blite_rc_car/14_blite_rc_car.ino
+++ examples/14_blite_rc_car/14_blite_rc_car.ino
@@ -1,4 +1,6 @@
-#include <blite.h>
+#include "blite.h"
+#include <ESP8266WebServer.h>
+#include <WebSocketsServer.h>
 #include "remote.h"
 #include <WebSocketsServer.h>
 
@@ -10,9 +12,77 @@
 
 Blite myBot;
 WebSocketsServer webSocket = WebSocketsServer(81);
+#define CMD_BACKWARD 2
+#define CMD_STOP 0
+#define CMD_FORWARD 1
+#define CMD_LEFT 4
+#define CMD_RIGHT 8
+#define CMD_PUSH 5
+#define CMD_SRVCLCK 6
+#define CMD_SRVACLCK 9
+
+
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t* payload, size_t length) {
+  switch (type) {
+    case WStype_DISCONNECTED:
+      Serial.printf("[%u] Disconnected!\n", num);
+      break;
+    case WStype_CONNECTED:
+      {
+        IPAddress ip = webSocket.remoteIP(num);
+        Serial.printf("[%u] Connected from %d.%d.%d.%d\n", num, ip[0], ip[1], ip[2], ip[3]);
+      }
+      break;
+    case WStype_TEXT:
+      //Serial.printf("[%u] Received text: %s\n", num, payload);
+      String angle = String((char*)payload);
+      int command = angle.toInt();
+      Serial.print("command: ");
+      Serial.println(command);
+
+      switch (command) {
+        case CMD_STOP:
+          Serial.println("Stop");
+         // mybot.stop();
+          break;
+        case CMD_FORWARD:
+          Serial.println("Move Forward");
+          mybot.moveForward();
+          break;
+        case CMD_BACKWARD:
+          Serial.println("Move Backward");
+          mybot.moveBackward();
+          break;
+        case CMD_LEFT:
+          Serial.println("Turn Left");
+          mybot.turnLeft();
+          break;
+        case CMD_RIGHT:
+          Serial.println("Turn Right");
+          mybot.turnRight();
+          break;
+        case CMD_PUSH:
+          Serial.println("Push button pressed");
+          //mybot.push();
+          break;
+        case CMD_SRVCLCK:
+          Serial.println("Turn servo clockwise");
+          //mybot.turnServoClockwise();
+          break;
+        case CMD_SRVACLCK:
+          Serial.println("Turn servo anti-clockwise");
+          //mybot.turnServoAntiClockwise();
+          break;
+        default:
+          Serial.println("Unknown command");
+      }
+
+      break;
+  }
+}
+
 
 void setup(){
-    myBot.setup();
     Serial.begin(115200);
     myBot.smartConnectWiFi();
     webSocket.begin();
@@ -70,4 +140,16 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t* payload, size_t length)
 
       break;
   }
+}
+        Serial.println("Web Server: received a web page request");
+    wifiRemoteControl.on("/", HTTP_GET, []() {
+    wifiRemoteControl.on("/", HTTP_GET, []() {
+    wifiRemoteControl.on("/", HTTP_GET, []() {
+        wifiRemoteControl.send(200, "text/html", html);
+    });
+    wifiRemoteControl.begin();
+}
+void loop(){
+    wifiRemoteControl.handleClient();
+    webSocket.loop();
 }
--- examples/14_blite_rc_car/remote.h
+++ examples/14_blite_rc_car/remote.h
@@ -11,15 +11,28 @@ button { text-align: center; font-size: 24px;}
 #container {
     margin-right: auto;
     margin-left: auto;
-  width: 400px; 
+    width: 400px; 
     height: 400px;
     position: relative;
     margin-bottom: 10px;
 }
+#container2 {
+    margin-right: 400px;
+    margin-left: 0px;
+    width: 100px; 
+    height: 100px;
+    position: relative;
+    margin-bottom: 10px;
+}
+
 div[class^='button'] { position: absolute; }
 .button_up, .button_down { width:214px; height:104px;}
 .button_left, .button_right { width:104px; height:214px;}
 .button_stop { width:178px; height:178px;}
+.button_push { width:80px; height:40px;}
+.button_servo_c { width:122px; height:32px;}
+.button_servo_a { width:160px; height:35px;}
+.button_servo { width:82px; height:23px;}
 .button_up {
     background: url('https://newbiely.com/images/tutorial/up_inactive.png') no-repeat; 
   background-size: contain; 
@@ -59,6 +72,41 @@ div[class^='button'] { position: absolute; }
     top: 200px;
   transform: translate(-50%, -50%);
 }
+
+.button_push {
+  background: none no-repeat; 
+  background-size: contain;
+    left:-62px;
+    top: 200px;
+  transform: translate(-50%, -50%);
+}
+
+.button_servo_c {
+  background: none no-repeat; 
+  background-size: contain;
+    right:-214px;
+    top: 230px;
+  transform: translate(-50%, -50%);
+}
+
+.button_servo_a {
+  background: none no-repeat; 
+  background-size: contain;
+    right:-270px;
+    top: 170px;
+  transform: translate(-50%, -50%);
+}
+
+.button_servo {
+  background: none no-repeat; 
+  background-size: contain;
+    right:-150px;
+    top: 200px;
+  transform: translate(-50%, -50%);
+}
+
+
+
 </style>
 <script>
 var CMD_STOP     = 0;
@@ -66,6 +114,9 @@ var CMD_FORWARD  = 1;
 var CMD_BACKWARD = 2;
 var CMD_LEFT     = 4;
 var CMD_RIGHT    = 8;
+var CMD_PUSH     = 5;
+var CMD_SRVCLCK  = 6;
+var CMD_SRVACLCK = 9;
 var img_name_lookup = {
   [CMD_STOP]:     "stop",
   [CMD_FORWARD]:  "up",
@@ -144,6 +195,20 @@ function mouse_up(event)
     event.stopPropagation();   
     event.preventDefault();    
 }
+
+function wc_onclickpush() 
+{   
+  send_command(CMD_PUSH);   
+}
+function wc_onclickservclck() 
+{   
+  send_command(CMD_SRVCLCK);   
+}
+function wc_onclickservaclck() 
+{   
+  send_command(CMD_SRVACLCK);   
+}
+
 function send_command(cmd) 
 {   
   if(ws != null)
@@ -157,16 +222,24 @@ window.onload = init;
 <body>
 <h2>ESP8266 - RC Car via Web</h2>
 <div id="container">
-  <div id="0" class="button_stop"></div>
+    <div id="0" class="button_stop"></div>
     <div id="1" class="button_up"></div>
     <div id="2" class="button_down"></div>
     <div id="8" class="button_right"></div>
     <div id="4" class="button_left"></div>
+    <!-- <div id="5" class="button_push"><button type="button" onclick="wc_onclickpush();">Push</button></div> -->
+    <!-- <div id="6" class="button_servo_c"><button type="button" onclick="wc_onclickservclck();">Clockwise</button></div> -->
+    <!-- <div id="7" class="button_servo" style="font-size:17px; color:blue">SERVO</button></div> -->
+    <!-- <div id="9" class="button_servo_a"><button type="button" onclick="wc_onclickservaclck();">AntiClockwise</button></div> -->
 </div>
 <p>
-WebSocket : <span id="ws_state" style="color:blue">closed</span><br>
+WebSocket : <span id="ws_state" style="color:blue">closed</span>
+<button id="wc_conn" type="button" onclick="wc_onclick();">Connect</button><br>
 </p>
-<button id="wc_conn" type="button" onclick="wc_onclick();">Connect</button>
+<button id="wc_push" type="button" onclick="wc_onclickpush();">Push</button>
+<h style="font-size:17px; color:blue">SERVO</h>
+<button id="wc_servclck" type="button" onclick="wc_onclickservclck();">Clockwise</button>
+<button id="wc_servaclck" type="button" onclick="wc_onclickservaclck();">AntiClockwise</button>
 <br>
 <br>
 <div class="sponsor">Sponsored by <a href="https://paulsayan.wixsite.com/buildybee">Buidybee</a></div>
